7bb62c730e363020c68d1e3a7166d13b68c40835
pkopy <pawel.kopycki@gmail.com>
2018-05-14 19:34:58 +0200

    before dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var jasmine = require('gulp-jasmine-phantom');
  
 gulp.task('styles', function () {
   return gulp.src('./sass/**/*.scss')
@@ -9,9 +11,23 @@ gulp.task('styles', function () {
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
  
 gulp.task('default', function () {
   gulp.watch('./sass/**/*.scss', ['styles']);
+  gulp.watch('./*.html').on('change', browserSync.reload)
+
+  browserSync.init({
+    server: "./"
+  });
+});
+
+gulp.task('tests', function () {
+	gulp.src('tests/spec/extraSpec.js')
+		.pipe(jasmine({
+			integration: true,
+			vendor: 'js/**/*.js'
+		}));
 });
